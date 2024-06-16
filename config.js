0df1665cc06bc60b7910893171d59fae852d6a07
Nikhil2138 <134707277+Nikhil2138@users.noreply.github.com>
2024-06-16 14:08:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeT0jbjX249NtgbWDs3e5R31Y8VG35Hkg",
+  authDomain: "e-ride-app-8e41a.firebaseapp.com",
+  projectId: "e-ride-app-8e41a",
+  storageBucket: "e-ride-app-8e41a.appspot.com",
+  messagingSenderId: "116480739953",
+  appId: "1:116480739953:web:0db39594aa6cc001f929d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
